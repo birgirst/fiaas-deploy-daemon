aa45ea2b56786287628f00de3d66eb87b09e9188
birgirst <birgir.stefansson@schibsted.com>
2019-02-25 16:47:15 +0100

    xxxy

--- bin/upload_release_channel.sh
+++ bin/upload_release_channel.sh
@@ -8,8 +8,8 @@ echo $1
 cat <<EOF > release_channel.json
 {
     "image": "fiaas/fiaas-deploy-daemon:$version",
-    "build": "https://travis.schibsted.io/finn/fiaas-deploy-daemon/builds/5965774",
-    "commit": "https://github.schibsted.io/finn/fiaas-deploy-daemon/compare/d84df2e5dce5...2ace0ea22afa",
+    "build": "https://fiaas.semaphoreci.com/jobs/$SEMAPHORE_JOB_ID"
+    "commit": "https://github.com/fiaas/fiaas-deploy-daemon/commit/$SEMAPHORE_GIT_SHA"
     "spec": "https://fiaas.github.com/releases/artifacts/$version/fiaas.yml",
     "updated": "$(date)"
 }
